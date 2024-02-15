20cd72b11e9190d70b8077461f504b9701aec1de
OscarRoa34 <oscar.roa03@uptc.edu.co>
2024-02-15 18:22:38 -0500

    Supongo

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import Controllers.ShowText;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ShowText st = new ShowText();
         st.showText();
     }
