150f76735532f4ec91a68d2ea8623738a6d5078b
Oscar Alejandro Roa Doncel <159280561+OscarRoa34@users.noreply.github.com>
2024-02-16 11:49:23 -0500

    PERRO VAMOS

--- src/App.java
+++ src/App.java
@@ -4,5 +4,23 @@ public class App {
     public static void main(String[] args) {
         ShowText st = new ShowText();
         st.showText();
+
+        public class app {
+
+ /* public static void main(String[] args) {
+    int a = 194;
+    int b = 183;
+
+    String a2 = Integer.toHexString(a);
+    String b2 = Integer.toHexString(b);
+
+    String c = a2 + b2;
+
+    System.out.println(Integer.parseInt(c, 16));
+
+     */
+  }
+}
+
     }
 }
