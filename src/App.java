e43ff26fecc7883b0af74d125cc6d1188ea87cb5
OscarRoa34 <oscar.roa03@uptc.edu.co>
2024-02-19 21:00:51 -0500

    Moral de perro

--- src/App.java
+++ src/App.java
@@ -1,26 +1,11 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import Controllers.ShowText;
 
 public class App {
     public static void main(String[] args) {
-        ShowText st = new ShowText();
-        st.showText();
-
-        public class app {
-
- /* public static void main(String[] args) {
-    int a = 194;
-    int b = 183;
-
-    String a2 = Integer.toHexString(a);
-    String b2 = Integer.toHexString(b);
-
-    String c = a2 + b2;
-
-    System.out.println(Integer.parseInt(c, 16));
-
-     */
-  }
-}
-
+        ShowText txt = new ShowText();
+        txt.showText();
     }
 }
--- src/Controllers/ShowText.java
+++ src/Controllers/ShowText.java
@@ -1,6 +1,7 @@
 package Controllers;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,30 +10,18 @@ public class ShowText {
     public void showText() {
         String rutaArchivo = "PERSON2.TXT";
 
-        try (FileReader fr = new FileReader(rutaArchivo);
-                BufferedReader br = new BufferedReader(fr)) {
+        try {
+            FileInputStream fis = new FileInputStream(rutaArchivo);
 
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] datos = linea.split("\\\\");
-
-                if (datos.length >= 6 && !datos[4].isEmpty() && !datos[5].isEmpty()) {
-                    String codigo = datos[0];
-                    String apellido = datos[2];
-                    String nombre = datos[1];
-                    String genero = datos[3];
-                    char pesoR = datos[4].charAt(0);
-                    char sueldoR = datos[5].charAt(0);
-
-                    int valorNumericoPeso = (int) pesoR;
-                    int valorNumericoSueldo = (int) sueldoR;
-
-                    System.out.printf("%-12s\t%-20s\t%-20s\t%-12s\t%-12d\t%-12d\n", codigo, apellido, nombre, genero,
-                            valorNumericoPeso, valorNumericoSueldo);
-                }
+            int byteLeido;
+            while ((byteLeido = fis.read()) != -1) {
+                System.out.print(Integer.toString(byteLeido, 16));
             }
+
+            fis.close();
         } catch (IOException e) {
-            System.err.println("Error al leer el archivo: " + e.getMessage());
+            e.printStackTrace();
         }
+
     }
 }
