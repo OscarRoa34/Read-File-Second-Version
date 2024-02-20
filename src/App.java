0a93933d7a2dd5cd077e8050292cf2bfc29408db
OscarRoa34 <oscar.roa03@uptc.edu.co>
2024-02-20 09:53:19 -0500

    Moral de guau guau

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import java.io.FileInputStream;
-import java.io.IOException;
 
 import Controllers.ShowText;
 
--- src/Controllers/ShowText.java
+++ src/Controllers/ShowText.java
@@ -1,27 +1,33 @@
 package Controllers;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class ShowText {
-
     public void showText() {
-        String rutaArchivo = "PERSON2.TXT";
 
         try {
-            FileInputStream fis = new FileInputStream(rutaArchivo);
-
+            FileInputStream fis = new FileInputStream("PERSON2.TXT");
             int byteLeido;
+            ArrayList<String> cadenas = new ArrayList<String>();
+            String cad = "";
             while ((byteLeido = fis.read()) != -1) {
-                System.out.print(Integer.toString(byteLeido, 16));
-            }
+                if (byteLeido != 92 && byteLeido != 10) {
+                    String hexDecimal = Integer.toHexString(byteLeido);
+                    cad += hexDecimal;
+                } else {
+                    cadenas.add(cad);
+                    cad = "";
+                }
 
+            }
+            Business business = new Business();
             fis.close();
+            System.out.println(cadenas.get(5));
+            System.out.println(Integer.parseInt(cadenas.get(5), 16));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
